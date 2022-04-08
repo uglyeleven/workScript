c6681d2b51c3c2ef4e998fa657e37a05df02da09
uglyeleven <uglyeleven11@gmail.com>
2022-04-08 22:43:49 +0800

    msg

--- photoToDir.py
+++ photoToDir.py
@@ -2,19 +2,23 @@ import os, shutil
 
 
 def main():
-    filePath = "G:\\Try"
-    dirPath = "G:\\Try"
+    filePath = input("请输入路径")
     fileNameList = getMap(filePath)
-    creatDir(fileNameList,dirPath)
-    moveFIle(dirPath)
+    creatDir(fileNameList,filePath)
+    moveFIle(filePath)
 
 def getMap(path):
     fileNameMap = {}
     filelist = os.listdir(path)
-    for file in filelist:
-        if file[:-6] not in fileNameMap and file[-1] == 'g':
-            fileNameMap[file[:-6]] = True
+    finalNameLen = 6
+    for fileName in filelist:
+        if len(fileName) == 13 + finalNameLen or len(fileName) == 12 + finalNameLen or len(fileName) == 8 + finalNameLen:  #8位、12位、13位条码
+            if fileName[:-finalNameLen] not in fileNameMap and os.path.splitext(fileName)[1] == '.jpg':
+                fileNameMap[fileName[:-finalNameLen]] = True
+
     return fileNameMap
+
+
 
 def creatDir(fileNamelist,path):
     for fileName in fileNamelist:#
@@ -22,10 +26,11 @@ def creatDir(fileNamelist,path):
 
 
 def moveFIle(path):
+    finalNameLen = 6
     filelist = os.listdir(path)
     for file in filelist:
-        if file[-1] == 'g' :        #如果文件是jpg文件，则将此jpg文件移入对应的文件夹
-            shutil.move(path +'\\'+ file, path + '\\' + file[:-6])
+        if os.path.splitext(file)[1] == '.jpg' :        #如果文件是jpg文件，则将此jpg文件移入对应的文件夹
+            shutil.move(path +'\\'+ file, path + '\\' + file[:-finalNameLen])
 
 
 
